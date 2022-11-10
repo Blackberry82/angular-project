87cc3ae7fa01e5a3b6b449bca3747d015a855fb0
Blackberry82 <morozir@gmail.com>
2022-11-10 21:28:56 +0200

    forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,64 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
-import {FormsModule} from "@angular/forms";
-import {RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
-import { UsersComponent } from './components/mainUsers/users/users.component';
-import { UserComponent } from './components/mainUsers/user/user.component';
-import { PostsComponent } from './components/mainPosts/posts/posts.component';
-import { PostComponent } from './components/mainPosts/post/post.component';
-import { CommentsComponent } from './components/mainComments/comments/comments.component';
-import { CommentComponent } from './components/mainComments/comment/comment.component';
-import { MainLayoytComponent } from './layout/main-layoyt/main-layoyt.component';
-import { HeaderComponent } from './components/header/header.component';
-import { UserDetailsComponent } from './components/mainUsers/user-details/user-details.component';
-import { PostDetailsComponent } from './components/mainPosts/post-details/post-details/post-details.component';
-import { CommentDetailsComponent } from './components/mainComments/comment-details/comment-details/comment-details.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { CarComponent } from './components/car/car.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
-const routes: Routes = [
-  {
-    path: '', component: MainLayoytComponent, children: [
-      {path: '', redirectTo: 'users', pathMatch: "full"},
-      {
-        path: 'users', component: UsersComponent, children: [
-          {path: ':id', component: UserDetailsComponent}
-        ]
-      },
-      {
-        path: 'posts', component: PostsComponent, children: [
-          {path: ':id', component: PostDetailsComponent}
-        ]
-      },
-      {
-        path: 'comments', component: CommentsComponent, children:[
-          {path: ':id', component: CommentDetailsComponent}
-        ]},
-    ]
-  }
-  ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    UserComponent,
-    PostsComponent,
-    PostComponent,
-    CommentsComponent,
-    CommentComponent,
-    MainLayoytComponent,
-    HeaderComponent,
-    UserDetailsComponent,
-    PostDetailsComponent,
-    CommentDetailsComponent
+    CarsComponent,
+    CarComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot(routes)
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -1,4 +1,1 @@
-export * from './user.interface';
-export * from './post.interface';
-export * from './comment.interface';
-
+export * from './car.interface';
